465de7d425ad69d8499a88de73e19451d682e816
Miguel <miguelenrique1805@gmail.com>
2022-11-10 20:03:41 -0500

    schedule third commit

--- src/components/schedule/ScheduleBorder.jsx
+++ src/components/schedule/ScheduleBorder.jsx
@@ -1,127 +1,37 @@
+import { courses } from '../../data/courses';
 import { ContainerSchedule } from './containerSchedule/ContainerSchedule';
 import { ContainerVoid } from './containerVoid/ContainerVoid';
 import styles from './styles.module.scss';
 
 export const ScheduleBorder = () => {
-  // const hours = [
-  //   '7:00 - 8:00',
-  //   '8:00 - 9:00',
-  //   '9:00 - 10:00',
-  //   '10:00 - 11:00',
-  //   '11:00 - 12:00',
-  //   '12:00 - 13:00',
-  //   '13:00 - 14:00',
-  //   '14:00 - 15:00',
-  //   '15:00 - 16:00',
-  //   '16:00 - 17:00',
-  //   '17:00 - 18:00',
-  //   '18:00 - 19:00',
-  //   '19:00 - 20:00',
-  //   '20:00 - 21:00',
-  // ];
-
-  const courses = [
-    {
-      id: '01',
-      name: 'Procesamiento de Lenguaje Natural',
-      days: {
-        monday: ['7:00 - 8:00', '8:00 - 9:00'],
-        tuesday: [],
-        wednesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'red',
-      dateInit: '5451727642724',
-      dateEnd: '5451727642724',
-    },
-    {
-      id: '02',
-      name: 'Programación I',
-      days: {
-        monday: [],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: ['8:00 - 9:00'],
-      },
-      color: 'black',
-    },
-    {
-      id: '03',
-      name: 'Programación I',
-      days: {
-        monday: [],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'green',
-    },
-    {
-      id: '04',
-      name: 'Programación I',
-      days: {
-        monday: ['7:00 - 8:00', '8:00 - 9:00'],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'pink',
-    },
-    {
-      id: '05',
-      name: 'Programación I',
-      days: {
-        monday: [],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'gold',
-    },
-    {
-      id: '06',
-      name: 'Programación I',
-      days: {
-        monday: ['7:00 - 8:00'],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'orange',
-    },
-    {
-      id: '07',
-      name: 'Programación I',
-      days: {
-        monday: ['7:00 - 8:00'],
-        tuesday: ['7:00 - 8:00', '8:00 - 9:00'],
-        wednesday: ['8:00 - 9:00'],
-        thursday: [],
-        friday: ['7:00 - 8:00'],
-        saturday: [],
-      },
-      color: 'blue',
-    },
+  const hours = [
+    '7:00 - 8:00',
+    '8:00 - 9:00',
+    '9:00 - 10:00',
+    '10:00 - 11:00',
+    '11:00 - 12:00',
+    '12:00 - 13:00',
+    '13:00 - 14:00',
+    '14:00 - 15:00',
+    '15:00 - 16:00',
+    '16:00 - 17:00',
+    '17:00 - 18:00',
+    '18:00 - 19:00',
+    '19:00 - 20:00',
+    '20:00 - 21:00',
   ];
 
-  const days = ['Horario', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
   return (
     <div className={styles['container']}>
       <div className={styles['container_schedule']}>
         <div className={styles['container_schedule_top']}></div>
         <div className={styles['container_schedule_header']}>
+          <div className={styles['container_schedule_header_horario']}>
+            <h1 className={styles['container_schedule_header_horario_text']}>
+              Horario
+            </h1>
+          </div>
           {days.map((value, key) => (
             <div key={key} className={styles['container_schedule_header_caps']}>
               <h1 className={styles['container_schedule_header_caps_text']}>
@@ -136,7 +46,146 @@ export const ScheduleBorder = () => {
           </div>
         </div>
         <div className={styles['container_schedule_lineTime']}>
-          <div className={styles['container_schedule_lineTime_caps']}>
+          {hours.map((daysVal, key) => (
+            <div className={styles['container_schedule_lineTime_caps']}>
+              <div className={styles['container_schedule_lineTime_caps_text']}>
+                <h1
+                  className={
+                    styles['container_schedule_lineTime_caps_text_hour']
+                  }
+                >
+                  {daysVal}
+                </h1>
+              </div>
+              <div className={styles['container_schedule_lineTime_caps_mini']}>
+                {Object.keys(courses[0].days).map((value) =>
+                  value === 'monday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_course']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.monday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : value === 'tuesday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_course']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.tuesday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : value === 'wednesday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_course']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.wednesday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : value === 'thursday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_course']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.thursday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : value === 'friday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_course']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.friday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : value === 'saturday' ? (
+                    <>
+                      <div
+                        className={
+                          styles['container_schedule_lineTime_caps_mini_courseSat']
+                        }
+                      >
+                        {courses.map((value, key) =>
+                          value.days.saturday.includes(`${daysVal}`) ? (
+                            <div>
+                              <ContainerSchedule
+                                title={value.name}
+                                backgroundColor={value.color}
+                              />
+                            </div>
+                          ) : null
+                        )}
+                      </div>
+                    </>
+                  ) : (
+                    <ContainerVoid
+                      className={
+                        styles['container_schedule_lineTime_caps_mini_course']
+                      }
+                    />
+                  )
+                )}
+              </div>
+            </div>
+          ))}
+
+          {/* <div className={styles['container_schedule_lineTime_caps']}>
             <div className={styles['container_schedule_lineTime_caps_text']}>
               <h1
                 className={styles['container_schedule_lineTime_caps_text_hour']}
@@ -404,7 +453,7 @@ export const ScheduleBorder = () => {
                 )
               )}
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
